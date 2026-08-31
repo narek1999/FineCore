a1ee5df839b20fb507bdcd9d85190854229afc41
Narek Avetisyan <av77yan@gmail.com>
2026-09-01 01:34:59 +0200

    add docker compose, postgresql, and initial flyway migration

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE accounts (
+                          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                          user_id UUID NOT NULL,
+                          name VARCHAR(100) NOT NULL,
+                          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
